eb8925f6c9a3d336e58213a99d859f7a1001cf2b
Joao Lopes <joao.lopes@ecosia.org>
2020-08-16 17:12:15 +0200

    introduce animation, auto resize

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,7 @@
 /// <reference path="./game.ts" />
 
 gameStart()
+
+window.onresize = (event: Event) => {
+  document.body.style.setProperty('--pixel-size', `${Math.round(window.outerHeight / 100)}px`)
+}
